a1415b5f8052fa032a4666b486c6b2f4f8102a1a
Sergey Orlov <s.orlov@javad.com>
2014-07-23 22:07:22 +0400

    add navigator function gave appropriate names for divs

--- main.js
+++ main.js
@@ -1,31 +1,20 @@
 jQuery( document ).ready(function( ) {
 
 	var last = $("#Home");
-	$("#myDiv").html("Home");
-
-
-	$("#Home").click(function() {
-			$("#myDiv").html("Home");
-			console.log($(this));
+	$("#myContainer").html("Home");
+	
+	var navigator = function(id, text){
+		$(id).click(function() {
+			$("#myContainer").html(text);			
 			last.removeClass("active")
 			$(this).addClass("active")
-			last = $(this);			
-		});
-		
-	$("#Profile").click(function() {
-			$("#myDiv").html("Profile");
-			$(this).addClass("active")
-			last.removeClass("active")
-			last = $(this);
-			
-		});
-
-	$("#Portfolio").click(function() {
-			$("#myDiv").html("Portfolio");
-			$(this).addClass("active")
-			last.removeClass("active")
 			last = $(this);			
 		});	
+	};
+	
+	navigator("#Home","Home");
+	navigator("#Profile","Profile");
+	navigator("#Portfolio","Portfolio");
 	
 });
 
