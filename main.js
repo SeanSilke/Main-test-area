8027300f6542bab8b3f93bd60fdc9d41e09eeb88
Sergey Orlov <s.orlov@javad.com>
2014-07-24 22:42:40 +0400

    load one Html file to another

--- main.js
+++ main.js
@@ -1,20 +1,25 @@
 jQuery( document ).ready(function( ) {
 
-	var last = $("#Home");
-	$("#myContainer").html("Home");
+	//var last = $("#Home");
+	//$("#myContainer").html("Home");
+	//
+	//var navigator = function(id, text){
+	//	$(id).click(function() {
+	//		$("#myContainer").html(text);			
+	//		last.removeClass("active")
+	//		$(this).addClass("active")
+	//		last = $(this);			
+	//	});	
+	//};
+	//
+	//navigator("#Home","Home");
+	//navigator("#Profile","Profile");
+	//navigator("#Portfolio","Portfolio");
 	
-	var navigator = function(id, text){
-		$(id).click(function() {
-			$("#myContainer").html(text);			
-			last.removeClass("active")
-			$(this).addClass("active")
-			last = $(this);			
-		});	
-	};
-	
-	navigator("#Home","Home");
-	navigator("#Profile","Profile");
-	navigator("#Portfolio","Portfolio");
+	// For proper work need to use python SimpleHTTPServer  "python -m SimpleHTTPServer 8000"
+	$("#myContainer").load( "list.html",function(e) {
+		console.log( "Load was performed.", e );
+		});
 	
 });
 
