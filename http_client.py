84f9cecfa9809d2431c70afc261385f88187726e
Sergey Orlov <s.orlov@javad.com>
2014-07-28 20:25:54 +0400

    connecting to tornado app on vm

--- http_client.py
+++ http_client.py
@@ -3,19 +3,9 @@
 import httplib
 
 # ip of receiver 172.30.0.42
-conn = httplib.HTTPConnection('172.30.0.42:80')
-conn.request("GET", "/index.html")
+conn = httplib.HTTPConnection('sergey-vn:8888')
+conn.request("GET", "")
 r1 = conn.getresponse()
-#print dir(r1)
-print r1.status, r1.reason,"\n", r1.getheaders()
-data1 = r1.read()
-print data1
+#print r1.status, r1.reason,"\n", r1.getheaders()
 
-"""
-conn.request("GET", "/parrot.spam")
-r2 = conn.getresponse()
-print r2.status, r2.reason
-
-data2 = r2.read()
-conn.close()
-"""
+print r1.read()
