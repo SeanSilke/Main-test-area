a0637e925e25a36c579782065b873ce2234c3475
Sergey Orlov <OrlovMail@gmail.com>
2014-09-09 21:48:14 +0400

    correct using of __init__ and json loads

--- main_server_2.py
+++ main_server_2.py
@@ -1,9 +1,13 @@
 import tornado.ioloop
 import tornado.web
 import tornado.websocket
+import json
 
 
 class WebSocketHandler(tornado.websocket.WebSocketHandler):
+	def __init__(self, *args, **kwargs):
+		super(tornado.websocket.WebSocketHandler, self).__init__(*args, **kwargs)		
+		self.test_text = "Hello world"
 
 	def on_close(self):
 		print "WebSocket is closed"
@@ -15,7 +19,8 @@ class WebSocketHandler(tornado.websocket.WebSocketHandler):
 		return True
 
 	def on_message(self, message):
-		print message
+		msg = json.loads(message)
+		print message, msg['type'], self.test_text
 		self.write_message(message)
 
 app = tornado.web.Application([(r'/websocket', WebSocketHandler)])
