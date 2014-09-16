c4ab471016a7762f5a51c9a6bd554099320255e7
Sergey Orlov <OrlovMail@gmail.com>
2014-09-16 18:08:05 +0400

    add connection status with the python server. disable login if there is no connection

--- js_client.js
+++ js_client.js
@@ -1,23 +1,40 @@
 "use strict";
 jQuery( document ).ready(function( ) {
-	var reciever_id = 1
-	var recievers_dict = {}
-	var ws = new WebSocket("ws://localhost:8888/websocket");
+	var reciever_id = 1;
+	var recievers_dict = {};
+	var ws = undefined;
 
-	ws.onmessage = function(message){		
-		var message = (JSON.parse(message.data));	
-		if (message.type == 'send'){				
-			var reciever_id = message.id
-			var reciever = recievers_dict[reciever_id]
-			reciever.r_print(message.data)
+	(function (){
+		var newWs = new WebSocket("ws://localhost:8888/websocket");
+
+		newWs.onclose = function(){
+			$('H3').html("No connection with a server")
+			$("fieldset").prop('disabled', true)
+			$('.main_panel').html('')
 		}
 
-		if (message.type == 'event' && message.data == "logged"){			
-			var reciever_id = message.id
-			var reciever = recievers_dict[reciever_id]
-			reciever.enable()
+		newWs.onopen = function(){
+			$("fieldset").removeAttr("disabled")
+			$('H3').html('Connected to the server')
 		}
-	};	
+
+		newWs.onmessage = function(message){
+			var message = (JSON.parse(message.data));
+			if (message.type == 'send'){
+				var reciever_id = message.id
+				var reciever = recievers_dict[reciever_id]
+				reciever.r_print(message.data)
+			}
+
+			if (message.type == 'event' && message.data == "logged"){
+					var reciever_id = message.id
+					var reciever = recievers_dict[reciever_id]
+					reciever.enable()
+			}
+		};
+
+		ws = newWs
+	})()
 
 	var login = function(){
 
@@ -72,38 +89,40 @@ jQuery( document ).ready(function( ) {
 					</fieldset>\
 				</div>')
 
-		var header = reciever_dom_elem.find(".header")		
+		var header = reciever_dom_elem.find(".header")
 		header.html("Receiver " + reciever_id)
 
 		var fieldset = reciever_dom_elem.find("fieldset")
-		var enable = function(){			
+		var enable = function(){
 			fieldset.removeAttr( "disabled" )
 		}
-		
-		
-		var output_field = reciever_dom_elem.find('.output_field')
-		var r_print = function(data){						
-			output_field.append(data + "<br>");			
-		}		
 
-		var enter_button = reciever_dom_elem.find(".enter_button")	
-		var input_field = reciever_dom_elem.find(".input_field")		
+
+		var output_field = reciever_dom_elem.find('.output_field')
+		var r_print = function(data){
+			output_field.append(data + "<br>");
+		}
+
+		var enter_button = reciever_dom_elem.find(".enter_button")
+		var input_field = reciever_dom_elem.find(".input_field")
 
 		enter_button.click(function () {
 			var text = input_field.val()
-			callback('send',text)				
+			callback('send',text)
 		})
 
-		var close_button = reciever_dom_elem.find(".close_button")
-		close_button.click(function () {
+		var close = function () {
 			reciever_dom_elem.remove()
-			callback('close',' ')			
-		})	
+			callback('close',' ')
+		}
+		var close_button = reciever_dom_elem.find(".close_button")
+		close_button.click(close)
 
 		var reciever = {
 			reciever_dom_elem:reciever_dom_elem,
 			r_print: r_print,
-			enable: enable
+			enable: enable,
+			close: close
 		}
 
 		recievers_dict[reciever_id] = reciever
