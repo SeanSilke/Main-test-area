441b12e0b329c7fd63caaafc1f470d95b487d54f
Sergey Orlov <OrlovMail@gmail.com>
2014-09-23 20:37:22 +0400

    rename and remove unneeded files

--- asyn_tcp_client.py
+++ asyn_tcp_client.py
@@ -2,12 +2,6 @@ import threading
 import socket 
 import time
 
-TCP_IP = '172.30.0.42'
-TCP_PORT = 8002
-my_buffer = ""
-
-COMMAND = 'print,/par/net/ip:on'
-
 def recv_async(socket,bytes, callback):
    def wrapper():
        data = socket.recv(bytes)
@@ -16,44 +10,47 @@ def recv_async(socket,bytes, callback):
    thread.setDaemon(True)
    thread.start()
 
-def send_messages(socket,message_list):
-	for message in message_list:
-		if message: socket.send(message + '\n')
-		time.sleep(1)
-		recv_async(socket,1000, my_print)
+def process_data(socket, finish_callback,data,buff,state):		
+	buff += data
+	print buff
+	if  state == 'login' and "login:" in buff:
+		socket.send('a' + '\n')			
+		buff = ''
+		state = "password"
+	if  state == "password" and "Password:" in buff:
+		socket.send('b' + '\n')
+		buff = ''
+		state  = 'approval'
+	if  state == 'approval' and "Logged in on" in buff:
+		state = 'success'
 
+	if state != 'success':
+		def callback(data):
+			process_data(socket,finish_callback,data,buff,state)
+		recv_async(socket,1024,callback)
+	else:
+		finish_callback()
 
-def add_to_buffer(data):
-	global my_buffer
-	my_buffer += data
-	print my_buffer
-
-def my_print(data):
-	print data
-
-def receiver_login(socket,login = 'a', password = 'b', callback):
+def asyn_receiver_login(socket,callback):	
+	buff = ''
 	state = 'login'
-	global my_buffer	
-	while state != 'success':
-		recv_async(socket,100,add_to_buffer)
-		if  state == 'login' and "login:" in my_buffer:
-			socket.send(login + '\n')			
-			my_buffer = ''
-			state = "password"
-		if  state == "password" and "Password:" in my_buffer:
-			socket.send(password + '\n')
-			my_buffer = ''
-			state  = 'approval'
-		if  state == 'approval' and "Logged in on" in my_buffer:
-			state = 'success'
+	def first_callback(data):		
+		process_data(socket, callback,data,buff,state)
+	recv_async(socket,1024,first_callback)
+
+TCP_IP = '172.30.0.42'
+TCP_PORT = 8002
+COMMAND = 'print,/par/net/ip:on'
 
 socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 socket.connect((TCP_IP, TCP_PORT))
 
-#receiver_login(socket)
+def login_callback():	
+	print 'connected'
+	socket.send(COMMAND + '\n')
+	def callback(data):
+		print data
+	recv_async(socket,1024,callback)
 
-#send_messages(socket,message_list = [COMMAND])
-
-print 'ioloop started'
-time.sleep(500)
-
+asyn_receiver_login(socket,login_callback)
+time.sleep(100)
