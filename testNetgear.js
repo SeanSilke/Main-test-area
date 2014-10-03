7ed1b00bcebc31198c590f11218de597b7aab242
Sergey Orlov <OrlovMail@gmail.com>
2014-10-03 16:03:28 +0400

    Add proper handling of channel errors: error on connect and disconnect error Add connect method to receiver move invoke on channel connect there

--- testNetgear.js
+++ testNetgear.js
@@ -14,17 +14,16 @@ jQuery( document ).ready(function( ) {
 		var host = $("#host").val()
 		var port = $("#port").val()
 		var password = $("#password").val()
-		var receiver = new reciever_constructor(host,port,password)
-		receiver.enable()
-		receiver.channel.Connect()
-		receiver.connected = true
+		var receiver = new reciever_constructor(host,port,password,id)
+		id += 1
+		receiver.Connect()
 		receiver.Read()
 	}
 
 	$("#login_button").click(setReciever)
 
 
-	var reciever_constructor = function (host,port,password){
+	var reciever_constructor = function (host,port,password,id){
 		var that = this
 		this.id = id
 		this.connected = false
@@ -39,16 +38,15 @@ jQuery( document ).ready(function( ) {
 		}
 		this.channel = new J.Gnss.Channel.Http(this.parameters)
 		this.channel.on("close",function(e){
-				console.log(e)
-				that.dom_elem.remove()
+				that.disable()
+				that.header.append(" " + e.errText)
 				that.connected = false
 				})
 
 		this.dom_elem = $($(".template").html())
 
 		this.header = this.dom_elem.find(".header")
-		this.header.html("Receiver " + id)
-		id+=1
+		this.header.html("Receiver " + this.id)
 
 		this.output_field = this.dom_elem.find(".output_field")
 		this.enter_button = this.dom_elem.find(".enter_button")
@@ -56,16 +54,31 @@ jQuery( document ).ready(function( ) {
 		this.close_button = this.dom_elem.find(".close_button")
 		this.input_field = this.dom_elem.find(".input_field")
 
-		this.enable = function(){
+		this.enable = function(bool){
 			var fieldset = this.dom_elem.find("fieldset")
-			fieldset.removeAttr( "disabled" )
+			fieldset.prop('disabled',false)
+		}
+
+		this.disable = function(){
+			var fieldset = this.dom_elem.find("fieldset")
+			fieldset.prop('disabled',true)
+		}
+
+		this.Connect = function(){
+			try{
+				this.channel.Connect()
+				this.connected = true
+				this.enable()
+			}
+			catch(e){
+				this.header.append(" " + e.name)
+			}
 		}
 
 		this.Read = njsCompile(function (data){
 			while(this.connected){
 				var response = this.channel.Read.yld(1000)
 				this.output_field.append(response);
-				console.log(this.id, "- id; Reading test")
 			}
 		})
 
