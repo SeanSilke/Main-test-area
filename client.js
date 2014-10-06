75802c45c002889db578df2988621a445386d081
Sergey Orlov <OrlovMail@gmail.com>
2014-10-06 17:46:08 +0400

    Add dummy html and js files for client and service write out objects that will be used in clients and in the service

---
+++ client.js
@@ -0,0 +1,4 @@
+console.log("Hello I'am client")
+console.log(N.Njs.Compile)
+console.log(N.Webio.Connection)
+console.log(N.Webio.TransportClient)
---
+++ service.js
@@ -0,0 +1,8 @@
+console.log("Hello I'am service")
+console.log(N.Webio.ServiceSessionObject)
+console.log(N.Webio.Connection)
+
+console.log(N.Webio.SimpleBus)
+console.log(N.Webio.TransportClient)
+
+console.log(N.Util.guid)
