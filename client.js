cceef32a5e93532fa8b6fe78e8e6962446036d8e
Sergey Orlov <OrlovMail@gmail.com>
2014-10-06 20:29:39 +0400

    Add example code from Pasha

--- client.js
+++ client.js
@@ -1,4 +1,31 @@
-console.log("Hello I'am client")
-console.log(N.Njs.Compile)
-console.log(N.Webio.Connection)
-console.log(N.Webio.TransportClient)
+// console.log("Hello I'am client")
+// console.log(N.Njs.Compile)
+// console.log(N.Webio.Connection)
+// console.log(N.Webio.TransportClient)
+
+Create = N.Njs.Compile(function(){
+	var connection = new N.Webio.Connection();
+	connection.Connect.yld('http://geoserver:8200/webio');
+	var transport = new N.Webio.TransportClient(connection);
+
+	var countDownService = transport.Session.yld({name: "myCountdown"})
+	console.log("countDownService", countDownService)
+
+	var res1 = countDownService.Method1.yld();
+	console.log("res1", res1);
+
+	var res2 = countDownService.Method1.yld({rabbit: 'hole'})
+	console.log('res2',res2);
+
+	countDownService.Method2.yld(
+		{
+			WebioCallback: new N.Webio.Callback(
+				function(data){
+					console.log('on WebioCallback', data);
+				}
+			)
+		}
+	)
+})
+
+Create()
--- service.js
+++ service.js
@@ -1,8 +1,48 @@
-console.log("Hello I'am service")
-console.log(N.Webio.ServiceSessionObject)
-console.log(N.Webio.Connection)
+// console.log("Hello I'am service")
+// console.log(N.Webio.ServiceSessionObject)
+// console.log(N.Webio.Connection)
 
-console.log(N.Webio.SimpleBus)
-console.log(N.Webio.TransportClient)
+// console.log(N.Webio.SimpleBus)
+// console.log(N.Webio.TransportClient)
 
-console.log(N.Util.guid)
+// console.log(N.Util.guid)
+
+var ServiceSessionClass = N.Webio.ServiceSessionObject.extend({
+	initialize: function(data){
+	},
+
+	Method1: function(data){
+		console.log("Service's method1")
+		return {hello:"world",data:data};
+	},
+
+	Method2: N.Njs.Compile(function(data){
+		this.runThread(data.WebioCallback);
+	}),
+
+	runThread: N.Njs.Compile(function(WebioCallback){
+		sleep.yld(1000);
+		WebioCallback.fire('1');
+		sleep.yld(1000);
+		WebioCallback.fire('2');
+		sleep.yld(1000);
+		WebioCallback.fire('3');
+	})
+})
+
+var Create = N.Njs.Compile(function () {
+	console.log("Now we will registrate session on the server")
+	var sessionName = "myCountdown";
+	var connection = new N.Webio.Connection();
+	connection.Connect.yld('http://geoserver:8200/webio');
+	var bus = new N.Webio.SimpleBus(sessionName, ServiceSessionClass);
+	var transport = new N.Webio.TransportClient(connection, bus);
+	transport.UpdateService.yld([{
+		name: sessionName,
+		uid: N.Util.guid(),
+		info: "Test Service"
+	}]);
+	console.log("Done");
+})
+
+Create()
