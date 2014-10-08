a89230b39aaf82c64a153253073763b883014fef
Sergey Orlov <OrlovMail@gmail.com>
2014-10-08 15:34:40 +0400

    Add Countdown function to service.js Store callback from clients in list and call them with the countdown count
    Send callback from client to service.
    Modifying field in client.html accordion
    to countdown in service

--- client.js
+++ client.js
@@ -3,25 +3,29 @@
 // console.log(N.Webio.Connection)
 // console.log(N.Webio.TransportClient)
 
+jQuery( document ).ready(function( ) {
+	window.counterField = $(".counter")
+})
+
 Create = N.Njs.Compile(function(){
 	var connection = new N.Webio.Connection();
 	connection.Connect.yld('http://geoserver:8200/webio');
 	var transport = new N.Webio.TransportClient(connection);
 
 	var countDownService = transport.Session.yld({name: "myCountdown"})
-	console.log("countDownService", countDownService)
+	// console.log("countDownService", countDownService)
 
-	var res1 = countDownService.Method1.yld();
-	console.log("res1", res1);
+	// var res1 = countDownService.Method1.yld();
+	// console.log("res1", res1);
 
-	var res2 = countDownService.Method1.yld({rabbit: 'hole'})
-	console.log('res2',res2);
+	// var res2 = countDownService.Method1.yld({rabbit: 'hole'})
+	// console.log('res2',res2);
 
 	countDownService.Method2.yld(
 		{
 			WebioCallback: new N.Webio.Callback(
 				function(data){
-					console.log('on WebioCallback', data);
+					counterField.html(data)
 				}
 			)
 		}
--- service.js
+++ service.js
@@ -7,31 +7,63 @@
 
 // console.log(N.Util.guid)
 
+var text = "Hello i'am text"
+var isCountdown = true
+var count = 1
+var callbackList = []
+
+
+var Countdown = N.Njs.Compile(function(){
+	// isCountdown = true;
+	while(isCountdown){
+		console.log(count);
+		sleep.yld(500);
+		count++;
+		// console.log(callbackList)
+		for (var id in callbackList){
+			// console.log(callbackList[id]);
+			callbackList[id].fire(count)
+		}
+	}
+	// isCountdown = false
+})
+
+Countdown()
+
 var ServiceSessionClass = N.Webio.ServiceSessionObject.extend({
 	initialize: function(data){
 	},
 
 	Method1: function(data){
-		console.log("Service's method1")
+		console.log("Service's method1", text)
 		return {hello:"world",data:data};
 	},
 
 	Method2: N.Njs.Compile(function(data){
-		this.runThread(data.WebioCallback);
+		// while(isCountdown){
+			console.log("Method2")
+			callbackList.push(data.WebioCallback)
+			// data.WebioCallback.fire(count)
+		// }
 	}),
 
-	runThread: N.Njs.Compile(function(WebioCallback){
-		sleep.yld(1000);
-		WebioCallback.fire('1');
-		sleep.yld(1000);
-		WebioCallback.fire('2');
-		sleep.yld(1000);
-		WebioCallback.fire('3');
-	})
+
+	// Method2: N.Njs.Compile(function(data){
+	// 	this.runThread(data.WebioCallback);
+	// }),
+
+	// runThread: N.Njs.Compile(function(WebioCallback){
+	// 	WebioCallback.fire('1');
+	// 	sleep.yld(1000);
+	// 	WebioCallback.fire('2');
+	// 	sleep.yld(1000);
+	// 	WebioCallback.fire('3');
+	// 	sleep.yld(1000);
+	// })
 })
 
 var Create = N.Njs.Compile(function () {
-	console.log("Now we will registrate session on the server")
+	// console.log("Now we will registrate session on the server")
 	var sessionName = "myCountdown";
 	var connection = new N.Webio.Connection();
 	connection.Connect.yld('http://geoserver:8200/webio');
@@ -42,7 +74,7 @@ var Create = N.Njs.Compile(function () {
 		uid: N.Util.guid(),
 		info: "Test Service"
 	}]);
-	console.log("Done");
+	// console.log("Done");
 })
 
 Create()
