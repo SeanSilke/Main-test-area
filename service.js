bf546263b90f581a4f12c0c4b904ae83b79e5410
Sergey Orlov <OrlovMail@gmail.com>
2014-10-09 14:37:25 +0400

    Remove unnecessary console log message make Countdown function actually  count down from 1 to 10 pass the invocation of countdown in the method  Method1 if the Countdown function is currently running

--- service.js
+++ service.js
@@ -1,69 +1,46 @@
-// console.log("Hello I'am service")
-// console.log(N.Webio.ServiceSessionObject)
-// console.log(N.Webio.Connection)
-
-// console.log(N.Webio.SimpleBus)
-// console.log(N.Webio.TransportClient)
-
-// console.log(N.Util.guid)
-
-var text = "Hello i'am text"
-var isCountdown = true
-var count = 1
+var isCountdown = false
+var count = 10
 var callbackList = []
 
 
 var Countdown = N.Njs.Compile(function(){
-	// isCountdown = true;
-	while(isCountdown){
+	count = 10
+	while(count){
 		console.log(count);
-		sleep.yld(500);
-		count++;
-		// console.log(callbackList)
+		count--;
 		for (var id in callbackList){
-			// console.log(callbackList[id]);
 			callbackList[id].fire(count)
 		}
+		sleep.yld(500);
 	}
-	// isCountdown = false
+	isCountdown = false
+	return;
 })
-
-Countdown()
 
 var ServiceSessionClass = N.Webio.ServiceSessionObject.extend({
 	initialize: function(data){
 	},
 
 	Method1: function(data){
-		console.log("Service's method1", text)
-		return {hello:"world",data:data};
+		console.log("Service's method1")
+		if (isCountdown){
+			return
+			// return {hello:"world",data:data}
+		}else{
+			isCountdown = true
+			Countdown() //Tread
+		}
+		console.log("isCountdown", isCountdown)
 	},
 
 	Method2: N.Njs.Compile(function(data){
-		// while(isCountdown){
 			console.log("Method2")
 			callbackList.push(data.WebioCallback)
-			// data.WebioCallback.fire(count)
-		// }
 	}),
 
-
-	// Method2: N.Njs.Compile(function(data){
-	// 	this.runThread(data.WebioCallback);
-	// }),
-
-	// runThread: N.Njs.Compile(function(WebioCallback){
-	// 	WebioCallback.fire('1');
-	// 	sleep.yld(1000);
-	// 	WebioCallback.fire('2');
-	// 	sleep.yld(1000);
-	// 	WebioCallback.fire('3');
-	// 	sleep.yld(1000);
-	// })
 })
 
 var Create = N.Njs.Compile(function () {
-	// console.log("Now we will registrate session on the server")
 	var sessionName = "myCountdown";
 	var connection = new N.Webio.Connection();
 	connection.Connect.yld('http://geoserver:8200/webio');
@@ -74,7 +51,6 @@ var Create = N.Njs.Compile(function () {
 		uid: N.Util.guid(),
 		info: "Test Service"
 	}]);
-	// console.log("Done");
 })
 
 Create()
