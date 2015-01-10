18b035e2d565b2e8b7003b8a9cce26f9ffcbc4aa
Sergey Orlov <OrlovMail@gmail.com>
2015-01-11 02:40:52 +0400

    Let a player guess the number game.

--- first.rb
+++ first.rb
@@ -1,1 +1,11 @@
-puts "hello"
+i = rand(10)
+while 1 do
+	puts "Please enter your number"
+	a = gets.chomp
+	a = a.to_i
+	puts "My Number is bigger than yours" if a < i
+	puts "My Number is smaller than yours" if a > i
+	break if a == i
+end
+
+puts "My Number is #{i}"
